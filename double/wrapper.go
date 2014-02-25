79969e18841b96ac123cf4b20ed1821a3616e731
Arne Vansteenkiste <a.vansteenkiste@gmail.com>
2014-02-25 20:56:39 +0100

    add double precession

---
+++ double/wrapper.go
@@ -0,0 +1,110 @@
+// Internally used package double provides low-level wrappers for libfftw3
+package double
+
+//#cgo CFLAGS: -std=gnu99 -DHAVE_CONFIG_H
+//#cgo CFLAGS: -fomit-frame-pointer -mavx -mtune=native -malign-double -fstrict-aliasing -fno-schedule-insns -ffast-math -O3
+//#cgo LDFLAGS: -lm
+//#include "fftw3.h"
+import "C"
+import "unsafe"
+
+// Wrapper for fftw_plan_many_dft.
+// Internal but exported for use by package fftw.
+func PlanManyDft(n []int, howmany int, in []complex128, inembed []int, istride, idist int,
+	out []complex128, onembed []int, ostride, odist int, sign int, flags uint) unsafe.Pointer {
+
+	cN := to32bit(n)
+	cInembed := to32bit(inembed)
+	cOnembed := to32bit(onembed)
+
+	p := C.fftw_plan_many_dft(
+		(C.int)(len(n)),
+		(*C.int)(unsafe.Pointer(&cN[0])),
+		(C.int)(howmany),
+		(*C.fftw_complex)(unsafe.Pointer(&in[0])),
+		(*C.int)(unsafe.Pointer(&cInembed[0])),
+		(C.int)(istride),
+		(C.int)(idist),
+		(*C.fftw_complex)(unsafe.Pointer(&out[0])),
+		(*C.int)(unsafe.Pointer(&cOnembed[0])),
+		(C.int)(ostride),
+		(C.int)(odist),
+		(C.int)(sign),
+		(C.uint)(flags))
+
+	return unsafe.Pointer(p)
+}
+
+// Wrapper for fftw_plan_many_dft_r2c.
+// Internal but exported for use by package fftw.
+func PlanManyDftR2C(n []int, howmany int, in []float64, inembed []int, istride, idist int,
+	out []complex128, onembed []int, ostride, odist int, flags uint) unsafe.Pointer {
+
+	cN := to32bit(n)
+	cInembed := to32bit(inembed)
+	cOnembed := to32bit(onembed)
+
+	p := C.fftw_plan_many_dft_r2c(
+		(C.int)(len(n)),
+		(*C.int)(unsafe.Pointer(&cN[0])),
+		(C.int)(howmany),
+		(*C.double)(unsafe.Pointer(&in[0])),
+		(*C.int)(unsafe.Pointer(&cInembed[0])),
+		(C.int)(istride),
+		(C.int)(idist),
+		(*C.fftw_complex)(unsafe.Pointer(&out[0])),
+		(*C.int)(unsafe.Pointer(&cOnembed[0])),
+		(C.int)(ostride),
+		(C.int)(odist),
+		(C.uint)(flags))
+
+	return unsafe.Pointer(p)
+}
+
+// Wrapper for fftw_plan_many_dft_c2r.
+// Internal but exported for use by package fftw.
+func PlanManyDftC2R(n []int, howmany int, in []complex128, inembed []int, istride, idist int,
+	out []float64, onembed []int, ostride, odist int, flags uint) unsafe.Pointer {
+
+	cN := to32bit(n)
+	cInembed := to32bit(inembed)
+	cOnembed := to32bit(onembed)
+
+	p := C.fftw_plan_many_dft_c2r(
+		(C.int)(len(n)),
+		(*C.int)(unsafe.Pointer(&cN[0])),
+		(C.int)(howmany),
+		(*C.fftw_complex)(unsafe.Pointer(&in[0])),
+		(*C.int)(unsafe.Pointer(&cInembed[0])),
+		(C.int)(istride),
+		(C.int)(idist),
+		(*C.double)(unsafe.Pointer(&out[0])),
+		(*C.int)(unsafe.Pointer(&cOnembed[0])),
+		(C.int)(ostride),
+		(C.int)(odist),
+		(C.uint)(flags))
+
+	return unsafe.Pointer(p)
+}
+
+// Wrapper for fftw_execute.
+// Internal but exported for use by package fftw
+func Execute(plan unsafe.Pointer) {
+	C.fftw_execute(C.fftw_plan(plan))
+}
+
+// Wrapper for fftw_destroy_plan.
+// Internal but exported for use by package fftw
+func DestroyPlan(plan unsafe.Pointer) {
+	C.fftw_destroy_plan(C.fftw_plan(plan))
+}
+
+// C.int is 32 bit even on a 64bit system, but Go int is 32 or 64 bit.
+// So we need to convert in order to pass C int arrays.
+func to32bit(a []int) []int32 {
+	b := make([]int32, len(a))
+	for i := range b {
+		b[i] = int32(a[i])
+	}
+	return b
+}
--- fftw.go
+++ fftw.go
@@ -3,6 +3,7 @@ package fftw
 import (
 	"fmt"
 	"github.com/barnex/fftw/float"
+	"github.com/barnex/fftw/double"
 	"unsafe"
 )
 
@@ -95,3 +96,107 @@ func(p*PlanC2R)Destroy(){
 	p.in = nil
 	p.out = nil
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Plan for complex128 to complex128 FFT
+type PlanZ2Z struct {
+	handle  unsafe.Pointer // holds the C.fftwf_plan
+	in, out []complex128 // pointers to data to avoid GC
+}
+
+// Wrapper for fftw_plan_many_dft:
+// 	http://www.fftw.org/doc/Advanced-Complex-DFTs.html#Advanced-Complex-DFTs
+func PlanManyZ2Z(n []int, howmany int, in []complex128, inembed []int, istride, idist int,
+	out []complex128, onembed []int, ostride, odist int, sign int, flags Flag) *PlanZ2Z {
+
+	p := double.PlanManyDft(n, howmany, in, inembed, istride, idist, out, onembed, ostride, odist, sign, uint(flags))
+	if p == nil {
+		panic(fmt.Errorf("invalid plan: n:%v, howmany:%v, inembed:%v, istride:%v, idist:%v, onembed:%v, ostride:%v, odist:%v, sign:%v, flags:%v", n, howmany, inembed, istride, idist, onembed, ostride, odist, sign, flags))
+	}
+	return &PlanZ2Z{unsafe.Pointer(p), in, out}
+}
+
+func(p*PlanZ2Z)Execute(){
+	double.Execute(p.handle)
+}
+
+func(p*PlanZ2Z)Destroy(){
+	double.DestroyPlan(p.handle)
+	p.handle = nil
+	p.in = nil
+	p.out = nil
+}
+
+// Plan for float64 to complex128 FFT
+type PlanD2Z struct {
+	handle unsafe.Pointer
+	in     []float64
+	out    []complex128
+}
+
+// Wrapper for fftw_plan_many_dft_r2c:
+// 	http://www.fftw.org/doc/Advanced-Real_002ddata-DFTs.html#Advanced-Real_002ddata-DFTs
+func PlanManyD2Z(n []int, howmany int, in []float64, inembed []int, istride, idist int,
+	out []complex128, onembed []int, ostride, odist int, flags Flag) *PlanD2Z {
+
+	p := double.PlanManyDftR2C(n, howmany, in, inembed, istride, idist, out, onembed, ostride, odist, uint(flags))
+	if p == nil {
+		panic(fmt.Errorf("invalid plan: n:%v, howmany:%v, inembed:%v, istride:%v, idist:%v, onembed:%v, ostride:%v, odist:%v, flags:%v", n, howmany, inembed, istride, idist, onembed, ostride, odist, flags))
+	}
+	return &PlanD2Z{unsafe.Pointer(p), in, out}
+}
+
+
+func(p*PlanD2Z)Execute(){
+	double.Execute(p.handle)
+}
+
+func(p*PlanD2Z)Destroy(){
+	double.DestroyPlan(p.handle)
+	p.handle = nil
+	p.in = nil
+	p.out = nil
+}
+
+type PlanZ2D struct {
+	handle unsafe.Pointer
+	in     []complex128
+	out    []float64
+}
+
+// Wrapper for fftw_plan_many_dft_c2r:
+// 	http://www.fftw.org/doc/Advanced-Real_002ddata-DFTs.html#Advanced-Real_002ddata-DFTs
+func PlanManyZ2D(n []int, howmany int, in []complex128, inembed []int, istride, idist int,
+	out []float64, onembed []int, ostride, odist int, flags Flag) *PlanZ2D {
+
+	p := double.PlanManyDftC2R(n, howmany, in, inembed, istride, idist, out, onembed, ostride, odist, uint(flags))
+	if p == nil {
+		panic(fmt.Errorf("invalid plan: n:%v, howmany:%v, inembed:%v, istride:%v, idist:%v, onembed:%v, ostride:%v, odist:%v, flags:%v", n, howmany, inembed, istride, idist, onembed, ostride, odist, flags))
+	}
+	return &PlanZ2D{unsafe.Pointer(p), in, out}
+}
+
+
+func(p*PlanZ2D)Execute(){
+	double.Execute(p.handle)
+}
+
+func(p*PlanZ2D)Destroy(){
+	double.DestroyPlan(p.handle)
+	p.handle = nil
+	p.in = nil
+	p.out = nil
+}
+
