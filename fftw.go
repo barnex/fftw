fe3751f8ce9506a11885aa6ed1888a08d1c82b94
Arne Vansteenkiste <a.vansteenkiste@gmail.com>
2014-02-25 21:48:14 +0100

    add PlanC2C

--- fftw.go
+++ fftw.go
@@ -2,34 +2,47 @@ package fftw
 
 import (
 	"fmt"
-	"github.com/barnex/fftw/float"
 	"github.com/barnex/fftw/double"
+	"github.com/barnex/fftw/float"
 	"unsafe"
 )
 
 // Plan for complex64 to complex64 FFT
-type PlanC2C struct {
+type C2CPlan struct {
 	handle  unsafe.Pointer // holds the C.fftwf_plan
-	in, out []complex64 // pointers to data to avoid GC
+	in, out []complex64    // pointers to data to avoid GC
 }
 
 // Wrapper for fftwf_plan_many_dft:
 // 	http://www.fftw.org/doc/Advanced-Complex-DFTs.html
 func PlanManyC2C(n []int, howmany int, in []complex64, inembed []int, istride, idist int,
-	out []complex64, onembed []int, ostride, odist int, sign int, flags Flag) *PlanC2C {
+	out []complex64, onembed []int, ostride, odist int, sign int, flags Flag) *C2CPlan {
 
 	p := float.PlanManyDft(n, howmany, in, inembed, istride, idist, out, onembed, ostride, odist, sign, uint(flags))
 	if p == nil {
 		panic(fmt.Errorf("invalid plan: n:%v, howmany:%v, inembed:%v, istride:%v, idist:%v, onembed:%v, ostride:%v, odist:%v, sign:%v, flags:%v", n, howmany, inembed, istride, idist, onembed, ostride, odist, sign, flags))
 	}
-	return &PlanC2C{unsafe.Pointer(p), in, out}
+	return &C2CPlan{unsafe.Pointer(p), in, out}
 }
 
-func(p*PlanC2C)Execute(){
+// Provides the functionality of fftwf_plan_dft:
+// 	http://www.fftw.org/doc/Complex-DFTs.html
+func PlanC2C(n []int, in []complex64, out []complex64, sign int, flags Flag) *C2CPlan {
+	howmany := 1
+	idist := 0
+	odist := 0
+	istride := 1
+	ostride := 1
+	inembed := n
+	onembed := n
+	return PlanManyC2C(n, howmany, in, inembed, istride, idist, out, onembed, ostride, odist, sign, flags)
+}
+
+func (p *C2CPlan) Execute() {
 	float.Execute(p.handle)
 }
 
-func(p*PlanC2C)Destroy(){
+func (p *C2CPlan) Destroy() {
 	float.DestroyPlan(p.handle)
 	p.handle = nil
 	p.in = nil
@@ -37,7 +50,7 @@ func(p*PlanC2C)Destroy(){
 }
 
 // Plan for float32 to complex64 FFT
-type PlanR2C struct {
+type R2CPlan struct {
 	handle unsafe.Pointer
 	in     []float32
 	out    []complex64
@@ -46,28 +59,27 @@ type PlanR2C struct {
 // Wrapper for fftwf_plan_many_dft_r2c:
 // 	http://www.fftw.org/doc/Advanced-Real_002ddata-DFTs.html
 func PlanManyR2C(n []int, howmany int, in []float32, inembed []int, istride, idist int,
-	out []complex64, onembed []int, ostride, odist int, flags Flag) *PlanR2C {
+	out []complex64, onembed []int, ostride, odist int, flags Flag) *R2CPlan {
 
 	p := float.PlanManyDftR2C(n, howmany, in, inembed, istride, idist, out, onembed, ostride, odist, uint(flags))
 	if p == nil {
 		panic(fmt.Errorf("invalid plan: n:%v, howmany:%v, inembed:%v, istride:%v, idist:%v, onembed:%v, ostride:%v, odist:%v, flags:%v", n, howmany, inembed, istride, idist, onembed, ostride, odist, flags))
 	}
-	return &PlanR2C{unsafe.Pointer(p), in, out}
+	return &R2CPlan{unsafe.Pointer(p), in, out}
 }
 
-
-func(p*PlanR2C)Execute(){
+func (p *R2CPlan) Execute() {
 	float.Execute(p.handle)
 }
 
-func(p*PlanR2C)Destroy(){
+func (p *R2CPlan) Destroy() {
 	float.DestroyPlan(p.handle)
 	p.handle = nil
 	p.in = nil
 	p.out = nil
 }
 
-type PlanC2R struct {
+type C2RPlan struct {
 	handle unsafe.Pointer
 	in     []complex64
 	out    []float32
@@ -76,63 +88,49 @@ type PlanC2R struct {
 // Wrapper for fftwf_plan_many_dft_c2r:
 // 	http://www.fftw.org/doc/Advanced-Real_002ddata-DFTs.html
 func PlanManyC2R(n []int, howmany int, in []complex64, inembed []int, istride, idist int,
-	out []float32, onembed []int, ostride, odist int, flags Flag) *PlanC2R {
+	out []float32, onembed []int, ostride, odist int, flags Flag) *C2RPlan {
 
 	p := float.PlanManyDftC2R(n, howmany, in, inembed, istride, idist, out, onembed, ostride, odist, uint(flags))
 	if p == nil {
 		panic(fmt.Errorf("invalid plan: n:%v, howmany:%v, inembed:%v, istride:%v, idist:%v, onembed:%v, ostride:%v, odist:%v, flags:%v", n, howmany, inembed, istride, idist, onembed, ostride, odist, flags))
 	}
-	return &PlanC2R{unsafe.Pointer(p), in, out}
+	return &C2RPlan{unsafe.Pointer(p), in, out}
 }
 
-
-func(p*PlanC2R)Execute(){
+func (p *C2RPlan) Execute() {
 	float.Execute(p.handle)
 }
 
-func(p*PlanC2R)Destroy(){
+func (p *C2RPlan) Destroy() {
 	float.DestroyPlan(p.handle)
 	p.handle = nil
 	p.in = nil
 	p.out = nil
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
 // Plan for complex128 to complex128 FFT
-type PlanZ2Z struct {
+type Z2ZPlan struct {
 	handle  unsafe.Pointer // holds the C.fftwf_plan
-	in, out []complex128 // pointers to data to avoid GC
+	in, out []complex128   // pointers to data to avoid GC
 }
 
 // Wrapper for fftw_plan_many_dft:
 // 	http://www.fftw.org/doc/Advanced-Complex-DFTs.html
 func PlanManyZ2Z(n []int, howmany int, in []complex128, inembed []int, istride, idist int,
-	out []complex128, onembed []int, ostride, odist int, sign int, flags Flag) *PlanZ2Z {
+	out []complex128, onembed []int, ostride, odist int, sign int, flags Flag) *Z2ZPlan {
 
 	p := double.PlanManyDft(n, howmany, in, inembed, istride, idist, out, onembed, ostride, odist, sign, uint(flags))
 	if p == nil {
 		panic(fmt.Errorf("invalid plan: n:%v, howmany:%v, inembed:%v, istride:%v, idist:%v, onembed:%v, ostride:%v, odist:%v, sign:%v, flags:%v", n, howmany, inembed, istride, idist, onembed, ostride, odist, sign, flags))
 	}
-	return &PlanZ2Z{unsafe.Pointer(p), in, out}
+	return &Z2ZPlan{unsafe.Pointer(p), in, out}
 }
 
-func(p*PlanZ2Z)Execute(){
+func (p *Z2ZPlan) Execute() {
 	double.Execute(p.handle)
 }
 
-func(p*PlanZ2Z)Destroy(){
+func (p *Z2ZPlan) Destroy() {
 	double.DestroyPlan(p.handle)
 	p.handle = nil
 	p.in = nil
@@ -140,7 +138,7 @@ func(p*PlanZ2Z)Destroy(){
 }
 
 // Plan for float64 to complex128 FFT
-type PlanD2Z struct {
+type D2ZPlan struct {
 	handle unsafe.Pointer
 	in     []float64
 	out    []complex128
@@ -149,28 +147,27 @@ type PlanD2Z struct {
 // Wrapper for fftw_plan_many_dft_r2c:
 // 	http://www.fftw.org/doc/Advanced-Real_002ddata-DFTs.html
 func PlanManyD2Z(n []int, howmany int, in []float64, inembed []int, istride, idist int,
-	out []complex128, onembed []int, ostride, odist int, flags Flag) *PlanD2Z {
+	out []complex128, onembed []int, ostride, odist int, flags Flag) *D2ZPlan {
 
 	p := double.PlanManyDftR2C(n, howmany, in, inembed, istride, idist, out, onembed, ostride, odist, uint(flags))
 	if p == nil {
 		panic(fmt.Errorf("invalid plan: n:%v, howmany:%v, inembed:%v, istride:%v, idist:%v, onembed:%v, ostride:%v, odist:%v, flags:%v", n, howmany, inembed, istride, idist, onembed, ostride, odist, flags))
 	}
-	return &PlanD2Z{unsafe.Pointer(p), in, out}
+	return &D2ZPlan{unsafe.Pointer(p), in, out}
 }
 
-
-func(p*PlanD2Z)Execute(){
+func (p *D2ZPlan) Execute() {
 	double.Execute(p.handle)
 }
 
-func(p*PlanD2Z)Destroy(){
+func (p *D2ZPlan) Destroy() {
 	double.DestroyPlan(p.handle)
 	p.handle = nil
 	p.in = nil
 	p.out = nil
 }
 
-type PlanZ2D struct {
+type Z2DPlan struct {
 	handle unsafe.Pointer
 	in     []complex128
 	out    []float64
@@ -179,24 +176,22 @@ type PlanZ2D struct {
 // Wrapper for fftw_plan_many_dft_c2r:
 // 	http://www.fftw.org/doc/Advanced-Real_002ddata-DFTs.html
 func PlanManyZ2D(n []int, howmany int, in []complex128, inembed []int, istride, idist int,
-	out []float64, onembed []int, ostride, odist int, flags Flag) *PlanZ2D {
+	out []float64, onembed []int, ostride, odist int, flags Flag) *Z2DPlan {
 
 	p := double.PlanManyDftC2R(n, howmany, in, inembed, istride, idist, out, onembed, ostride, odist, uint(flags))
 	if p == nil {
 		panic(fmt.Errorf("invalid plan: n:%v, howmany:%v, inembed:%v, istride:%v, idist:%v, onembed:%v, ostride:%v, odist:%v, flags:%v", n, howmany, inembed, istride, idist, onembed, ostride, odist, flags))
 	}
-	return &PlanZ2D{unsafe.Pointer(p), in, out}
+	return &Z2DPlan{unsafe.Pointer(p), in, out}
 }
 
-
-func(p*PlanZ2D)Execute(){
+func (p *Z2DPlan) Execute() {
 	double.Execute(p.handle)
 }
 
-func(p*PlanZ2D)Destroy(){
+func (p *Z2DPlan) Destroy() {
 	double.DestroyPlan(p.handle)
 	p.handle = nil
 	p.in = nil
 	p.out = nil
 }
-
--- fftw_test.go
+++ fftw_test.go
@@ -5,6 +5,27 @@ import (
 	"fmt"
 )
 
+func ExamplePlanC2C() {
+	N := 8
+	data := make([]complex64, N)
+
+	n := []int{N}
+	plan := PlanC2C(n, data, data, FORWARD, ESTIMATE)
+	defer plan.Destroy()
+
+	data[0] = 1
+	fmt.Println(data)
+	plan.Execute()
+	fmt.Println(data)
+	plan.Execute()
+	fmt.Println(data)
+
+	// Output:
+	// [(1+0i) (0+0i) (0+0i) (0+0i) (0+0i) (0+0i) (0+0i) (0+0i)]
+	// [(1+0i) (1+0i) (1+0i) (1+0i) (1+0i) (1+0i) (1+0i) (1+0i)]
+	// [(8+0i) (0+0i) (0+0i) (0+0i) (0+0i) (0+0i) (0+0i) (0+0i)]
+}
+
 func ExamplePlanManyC2C() {
 	N := 8
 	data := make([]complex64, N)
