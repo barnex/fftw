43a0191d372a72717be01e28c2a3b459820b6912
Arne Vansteenkiste <a.vansteenkiste@gmail.com>
2014-02-27 13:18:56 +0100

    thread doc

--- threads.go
+++ threads.go
@@ -5,11 +5,15 @@ import(
 	"github.com/barnex/fftw/float"
 )
 
+// InitThreads should be called once to initialize multi-threaded FFTW. See:
+// 	http://www.fftw.org/doc/Usage-of-Multi_002dthreaded-FFTW.html
 func InitThreads() {
 	float.InitThreads()
 	double.InitThreads()
 }
 
+// PlanWithNThreads(n) causes all subsequent plans to use at most n threads. See:
+// 	http://www.fftw.org/doc/Usage-of-Multi_002dthreaded-FFTW.html
 func PlanWithNThreads(nthreads int) {
 	float.PlanWithNThreads(nthreads)
 	double.PlanWithNThreads(nthreads)
