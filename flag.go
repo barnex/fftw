fbeb20e90ad49c90bc7f2a1b86f4c3aa053c2c2d
Arne Vansteenkiste <barnex@gmail.com>
2014-11-17 11:41:49 +0100

    document flag

--- flag.go
+++ flag.go
@@ -12,15 +12,15 @@ import "fmt"
 type Flag uint
 
 const (
-	MEASURE         Flag = C.FFTW_MEASURE
-	DESTROY_INPUT   Flag = C.FFTW_DESTROY_INPUT
-	UNALIGNED       Flag = C.FFTW_UNALIGNED
 	CONSERVE_MEMORY Flag = C.FFTW_CONSERVE_MEMORY
-	EXHAUSTIVE      Flag = C.FFTW_EXHAUSTIVE
-	PRESERVE_INPUT  Flag = C.FFTW_PRESERVE_INPUT
-	PATIENT         Flag = C.FFTW_PATIENT
-	ESTIMATE        Flag = C.FFTW_ESTIMATE
-	WISDOM_ONLY     Flag = C.FFTW_WISDOM_ONLY
+	DESTROY_INPUT   Flag = C.FFTW_DESTROY_INPUT // An out-of-place transform is allowed to overwrite its input array with arbitrary data.
+	ESTIMATE        Flag = C.FFTW_ESTIMATE // Instead of time measurements, a simple heuristic is used to pick a plan quickly. The input/output arrays are not overwritten during planning.
+	EXHAUSTIVE      Flag = C.FFTW_EXHAUSTIVE // Like FFTW_PATIENT, but considers an even wider range of algorithms.
+	MEASURE         Flag = C.FFTW_MEASURE // Find an optimized plan by computing several FFTs and measuring their execution time. 	
+	PATIENT         Flag = C.FFTW_PATIENT // Like FFTW_MEASURE, but considers a wider range of algorithms.
+	PRESERVE_INPUT  Flag = C.FFTW_PRESERVE_INPUT // An out-of-place transform must not change its input array.
+	UNALIGNED       Flag = C.FFTW_UNALIGNED // The algorithm may not impose any unusual alignment requirements on the input/output arrays.
+	WISDOM_ONLY     Flag = C.FFTW_WISDOM_ONLY // The plan is only created if wisdom is available for the given problem. 
 )
 
 func (f Flag) String() string {
@@ -38,12 +38,13 @@ const (
 )
 
 var flagName = map[Flag]string{
-	MEASURE:         "MEASURE",
-	DESTROY_INPUT:   "DESTROY_INPUT",
-	UNALIGNED:       "UNALIGNED",
 	CONSERVE_MEMORY: "CONSERVE_MEMORY",
-	EXHAUSTIVE:      "EXHAUSTIVE",
-	PRESERVE_INPUT:  "PRESERVE_INPUT",
-	PATIENT:         "PATIENT",
+	DESTROY_INPUT:   "DESTROY_INPUT",
 	ESTIMATE:        "ESTIMATE",
-	WISDOM_ONLY:     "WISDOM_ONLY"}
+	EXHAUSTIVE:      "EXHAUSTIVE",
+	MEASURE:         "MEASURE",
+	PATIENT:         "PATIENT",
+	PRESERVE_INPUT:  "PRESERVE_INPUT",
+	UNALIGNED:       "UNALIGNED",
+	WISDOM_ONLY:     "WISDOM_ONLY",
+}
